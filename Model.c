29705796c63abdb37bf2d1893b37aab444384da7
yugoo <uosuj461@mtroyal.ca>
2019-02-22 13:03:51 -0700

    implemented Makefile and also made a .h file for the Raster library

---
+++ Model.c
@@ -0,0 +1,50 @@
+#include <model.h>
+void player_set_direction(struct Player *player, int direction) {
+    player->direction = direction;
+}
+
+void player_set_speed(struct Player *player, int speed) {
+    if( speed <= player->max_speed ) {
+        player->speed = speed;
+    }
+}
+
+bool player_take_damage(struct Player *player, int damage) {
+    bool dead = false;
+    if ( player->health <= damage ) {
+        dead = true;
+    }
+    player->health =- damage;
+    return dead;
+}
+
+void player_reload(struct Player *player) {
+    unsigned int ammo_needed;
+    ammo_needed = player->max_magazine - player->magazine;
+    if ( ammo_needed > 0 ) {
+        player->magazine += ammo_needed;
+        player->ammo =- ammo_needed;
+    }
+}
+
+void player_max_ammo(strcut Player *player) {
+    player->ammo = player->max_ammo;
+}
+
+void zombie_set_speed(struct Zombie *zombie, int speed){
+    if(speed <= zombie-> max_speed){
+      zombie->speed = speed;
+    }
+}
+
+void zombie_strength(struct Zombie *zombie, int strength){
+  zombie->strength = strenght;
+}
+
+bool zombie_take_damage(struct Zombie * zombie int damage ){
+  bool dead = false;
+    if(zombie->health <= damage){
+        dead = true;
+    }
+    zombie->health -= damage;
+}
